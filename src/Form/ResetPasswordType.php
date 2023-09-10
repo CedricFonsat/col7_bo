81da3d69c29289e7b2a74aa56124e4c0b71eeaf2
cedricfonsat <79462084+CedricFonsat@users.noreply.github.com>
2023-09-10 14:52:07 +0200

    modif

--- src/Form/ResetPasswordType.php
+++ src/Form/ResetPasswordType.php
@@ -13,7 +13,7 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ResetPasswordType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('password', PasswordType::class, [
